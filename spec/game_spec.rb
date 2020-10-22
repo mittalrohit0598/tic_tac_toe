39b13d43de2cecbab43da8acf16ee7818eb36a3f
mittalrohit0598 <mittalrohit0598@gmail.com>
2020-10-22 18:04:59 +0530

    Add tests for methods important to play the game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,20 +3,49 @@
 require './lib/tic_tac_toe/game'
 
 describe Game do
+  let(:game) { Game.new(%w[player1 player2]) }
   describe '#initialize' do
     it 'when arguments are absent' do
       expect { Game.new }.to raise_error ArgumentError
     end
     it 'when arguments are present' do
-      expect { Game.new(['player1, plaer2']) }.to_not raise_error
+      expect { game }.to_not raise_error
     end
   end
   describe '#switch_players' do
     it 'swaps current_player and other_player' do
-      game = Game.new(%w[player1 player2])
       players = game.current_player, game.other_player
       game.send(:switch_players)
       expect([game.other_player, game.current_player]).to eq(players)
+    end
+  end
+  describe '#move' do
+    it 'returns coordinates for the respective numbers' do
+      allow(game).to receive(:solicit_move)
+      allow(game).to receive(:gets).and_return('3')
+      move = game.send(:move)
+      expect(move).to eq([0, 2])
+    end
+    it 'run loop untill a valid move is entered' do
+      allow(game).to receive(:solicit_move)
+      allow(game).to receive(:gets).and_return('10')
+      allow(game).to receive(:puts)
+      allow(game).to receive(:gets).and_return('4')
+      move = game.send(:move)
+      expect(move).to eq([1, 0])
+    end
+  end
+  describe '#invalid_move?' do
+    describe 'returns false when move is between 1 and 9' do
+      it 'returns false when move is 1' do
+        expect(game.send(:invalid_move?, 1)).to eq(false)
+      end
+      it 'returns false when move is 9' do
+        expect(game.send(:invalid_move?, 1)).to eq(false)
+      end
+    end
+    it 'returns true when move is not between 1 and 9' do
+      expect(game.send(:invalid_move?, 10)).to eq(true)
     end
   end
 end
